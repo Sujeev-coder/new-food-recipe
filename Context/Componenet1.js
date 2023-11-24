38228f3c599a16cd778ffc5098d11d8f20f6aa7e
Sujeev john Thotakuri <sujeevjohnthotakuri@gmail.com>
2023-11-24 11:12:18 +0530

    initial

---
+++ Context/Componenet1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Componenet1 = () => {
+  return (
+    <div>Componenet1</div>
+  )
+}
+
+export default Componenet1
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>sujeev</div>
+  )
+}
+
+export default page
